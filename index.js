eb03d5da3c8c6407a51d57c89a6260dc851f3309
pr199 <pallesoneepiee@gmail.com>
2021-04-26 08:35:24 -0400

    corrections

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function getNote id(){
-    let noteObject = getExistingNotes()
-    if (!noteObject){
-        return 1
+function getNoteId() {
+    let noteObject = getExistingNotes();
+    if(!noteObject) {
+      return 1;
     }
-    const keysArrey = object.keys (noteobject)
-    const numberkeys = keysArrey.map((key)=>numberkeys(key))
-    console.log(number keys)
-    return Math.max(...numberkeys) +1
-}
-function getExistingNotes (){
-    let Notes = local Storage.getItem('notes')
-    if (!notes) {
-        return null
+    const keysArray = Object.keys(noteObject);
+    const numberKeys = keysArray.map((key) => Number(key));
+    console.log(numberKeys);
+    return Math.max(...numberKeys) + 1;
+  } 
+  function getExistingNotes() {
+    let notes = localStorage.getItem("notes");
+    if(!notes) {
+        return null;
     }
-    return JSON.parse(notes)
-}
+    return JSON.parse(notes);
+  }
